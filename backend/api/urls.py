e4e39c475630ca10bfedda9b6f14f379116ac5ff
Wachira-Dickson <dicksonwachira886@gmail.com>
2026-07-07 11:04:54 +0300

    Create backend apis using django rest framework

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
 from django.urls import path
+from .views import *
+from rest_framework.routers import DefaultRouter
 
-urlpatterns = [
-    
-]
+router = DefaultRouter()
+router.register('countries', CountryViewset, basename='country')
+router.register('leagues', LeagueViewset, basename='league')
+router.register('characteristics', CharacteristicViewset, basename='characteristic')
+urlpatterns = router.urls
--- backend/api/views.py
+++ backend/api/views.py
@@ -1,3 +1,37 @@
 from django.shortcuts import render
+from rest_framework import viewsets, permissions
+from .serializers import *
+from .models import *
+from rest_framework.response import Response
+
+class CountryViewset(viewsets.ViewSet):
+    queryset = Country.objects.all()
+    serializer_class = CountrySerializer
+    permission_classes = [permissions.AllowAny]
+
+    def list(self, request):
+        queryset = Country.objects.all()
+        serializer = self.serializer_class(queryset, many=True)
+        return Response(serializer.data)
+    
+class LeagueViewset(viewsets.ViewSet):
+    queryset = League.objects.all()
+    serializer_class = LeagueSerializer
+    permission_classes = [permissions.AllowAny]
+
+    def list(self, request):
+        queryset = League.objects.all()
+        serializer = self.serializer_classerializer(queryset, many=True)
+        return Response(serializer.data)
+    
+class CharacteristicViewset(viewsets.ViewSet):
+    queryset = Characteristic.objects.all()
+    serializer_class = CharacteristicSerializer
+    permission_classes = [permissions.AllowAny]
+
+    def list(self, request):
+        queryset = Characteristic.objects.all()
+        serializer = self.serializer_class(queryset, many=True)
+        return Response(serializer.data)
 
 # Create your views here.
